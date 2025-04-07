b9244de8ee5ee7d2df252e51f0b41c3bc8925b4f
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-07 14:57:47 +0200

    palindromia

--- Palindromia/js/main.js
+++ Palindromia/js/main.js
@@ -2,8 +2,26 @@
 
 // Chiedo all'utente di scrivere una parola qualsiasi
 const userWord = prompt(`Inserisci una parola:`);
+console.log(`La tua parola è: ` + userWord);
+let userReverseWord = ``;
 
 // # SVOLGIMENTO
 
 // Dichiaro una funzione per determinare se la parola inserita è palindroma
 
+function isPalindrome(userWord, userReverseWord) {
+    for(let i = userWord.length - 1; i >= 0; i--){
+        const currentLetter = userWord[i];
+        userReverseWord += currentLetter;
+    }
+
+    console.log(`La tua parola al contrario è: ` + userReverseWord);
+
+    if(userWord === userReverseWord) return true;
+
+    return false;
+}
+
+// # OUTPUT
+
+console.log(`La tua parola è palindroma? `, isPalindrome(userWord, userReverseWord));
