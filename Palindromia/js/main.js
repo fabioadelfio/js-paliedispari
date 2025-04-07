2311d64ac91cda77b05c2abeef63ffee87cea002
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-07 14:20:30 +0200

    first commit

---
+++ Palindromia/js/main.js
@@ -0,0 +1,9 @@
+// # RACCOLTA DATI
+
+// Chiedo all'utente di scrivere una parola qualsiasi
+const userWord = prompt(`Inserisci una parola:`);
+
+// # SVOLGIMENTO
+
+// Dichiaro una funzione per determinare se la parola inserita è palindroma
+
